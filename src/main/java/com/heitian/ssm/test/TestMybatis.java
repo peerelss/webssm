974e16c1b6f347da78f2623ee25a7a7bfbeb8348
xiepeerless@gmail.com <xiepeerless@gmail.com>
2017-03-15 10:26:17 +0800

    add util

--- src/main/java/com/heitian/ssm/test/TestMybatis.java
+++ src/main/java/com/heitian/ssm/test/TestMybatis.java
@@ -1,5 +1,6 @@
 package com.heitian.ssm.test;
 
+import com.heitian.ssm.dao.TumblrDao;
 import com.heitian.ssm.model.TumblrModel;
 import com.heitian.ssm.service.TumblrService;
 import org.apache.ibatis.session.SqlSession;
@@ -10,19 +11,28 @@ import javax.annotation.Resource;
 import java.io.InputStream;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 /**
  * Created by root on 17-3-8.
  */
+
+@RunWith(SpringJUnit4ClassRunner.class)     //表示继承了SpringJUnit4ClassRunner类
+@ContextConfiguration(locations = {"classpath:root.xml"})
 public class TestMybatis {
-    @Resource
-    public static TumblrService mTumblrService;
-    public static void main(String[] args) {
-        getAllTumblr();
-    }
-    public static void getAllTumblr(){
-        List<TumblrModel> list=mTumblrService.getAllTumblr();
-        for(TumblrModel tumblrModel:list){
-            System.out.println(tumblrModel.getId());
-        }
+    private static Logger logger=Logger.getLogger(TestMybatis.class);
+    @Autowired
+    private TumblrDao mTumblrDao;
+    @Test
+    public void test1(){
+        TumblrModel tumblrModel=mTumblrDao.selectTumblrById(1);
+        System.out.println(tumblrModel.getId());
     }
 }
