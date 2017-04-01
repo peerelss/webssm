cb8000f856b91faa6099c3b579dd6df92d4e3675
xiepeerless@gmail.com <xiepeerless@gmail.com>
2017-04-01 13:48:41 +0800

    add util

--- SqlManager/src/com/kevin/Shell.java
+++ SqlManager/src/com/kevin/Shell.java
@@ -1,7 +1,14 @@
 package com.kevin;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by root on 17-3-27.
  */
 public class Shell {
+    public static void main(String[] args){
+        Map<String,Integer> map=new HashMap<>();
+        System.out.println(map.get("str"));
+    }
 }
